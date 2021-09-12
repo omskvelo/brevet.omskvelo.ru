f2fbfc4d43e32ef0aa8de8bb46635fbedc05daba
ROMAN <oatsmoke@bk.ru>
2021-09-12 18:19:29 +0600

    обновление результатов, в общие результаты добавлены итого км и ср участника

--- src/app/total-results/total-results.component.ts
+++ src/app/total-results/total-results.component.ts
@@ -3,6 +3,7 @@ import {take} from 'rxjs/operators';
 import {JsonData} from '../result/result.component';
 import {HttpClient} from '@angular/common/http';
 import {BrevetsData} from '../main/main.component';
+import {MatDialog} from '@angular/material/dialog';
 
 @Component({
   selector: 'app-total-results',
@@ -15,12 +16,10 @@ export class TotalResultsComponent implements OnInit {
   name = [];
   total = [];
 
-  constructor(private httpClient: HttpClient) {
+  constructor(private httpClient: HttpClient, private dialog: MatDialog) {
   }
 
   ngOnInit(): void {
-    //alert(document.getElementById('table').offsetWidth.toString());
-    //document.getElementById('total').style.width = (document.getElementById('table').offsetWidth).toString() + 'px';
     document.getElementById('total').style.height = (window.innerHeight - 64).toString() + 'px';
     this.httpClient.get('assets/data/brevetsData.json').pipe(take(1)).subscribe((data: BrevetsData) => {
       this.brevetsData = data;
@@ -49,7 +48,7 @@ export class TotalResultsComponent implements OnInit {
       i = 0;
       for (const item of this.name) {
         this.total[i] = [item];
-        for (j = 1; j <= this.jsonData.length; j++) {
+        for (j = 1; j <= this.jsonData.length + 2; j++) {
           this.total[i][j] = '-';
         }
         i++;
@@ -68,7 +67,42 @@ export class TotalResultsComponent implements OnInit {
         }
         j++;
       }
-      console.log(this.total);
+      let totalKM = 0;
+      let SR = [0, 0, 0, 0];
+      for (const item of this.total) {
+        totalKM = 0;
+        SR = [0, 0, 0, 0];
+        for (j = 1; j < item.length; j++) {
+          if (item[j] !== '-' && item[j] !== 'DNS' && item[j] !== 'DNF' && item[j] !== 'DNQ' && item[j] !== 'OTL') {
+            totalKM = totalKM + this.brevetsData[j - 1].distance;
+            switch (this.brevetsData[j - 1].distance) {
+              case 200: {
+                SR[0] = 1;
+                break;
+              }
+              case 300: {
+                SR[1] = 1;
+                break;
+              }
+              case 400: {
+                SR[2] = 1;
+                break;
+              }
+              case 600: {
+                SR[3] = 1;
+                break;
+              }
+            }
+          }
+        }
+        item[this.jsonData.length + 1] = totalKM;
+        if (SR[0] === 1 && SR[1] === 1 && SR[2] === 1 && SR[3] === 1) {
+          item[this.jsonData.length + 2] = 'СР';
+        }
+      }
     });
+  }
+  dialogClose(): any {
+    this.dialog.closeAll();
   }
 }
