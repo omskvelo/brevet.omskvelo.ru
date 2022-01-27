ffe5db4f7d03a2fe4f5ed08f4b2e9b78c082b219
Avermakov <avermakov@list.ru>
2022-01-27 15:53:05 +0600

    Add dropdown search

--- brevet/brevet_database/admin.py
+++ brevet/brevet_database/admin.py
@@ -1,13 +1,18 @@
+from re import search
 from django.contrib import admin
 
 from .models import Club, Randonneur, Route, Event, Result, Application
 
+class ResultAdmin(admin.ModelAdmin):
+    autocomplete_fields = ['randonneur']
+
+class RandonneurAdmin(admin.ModelAdmin):
+    search_fields = ['russian_surname', 'russian_name']
+
 admin.site.register(Club)
-admin.site.register(Randonneur)
+admin.site.register(Randonneur, RandonneurAdmin)
 admin.site.register(Route)
 admin.site.register(Event)
-admin.site.register(Result)
+admin.site.register(Result, ResultAdmin)
 admin.site.register(Application)
 
-
-# Register your models here.
--- brevet/users/admin.py
+++ brevet/users/admin.py
@@ -3,5 +3,8 @@ from django.contrib.auth.admin import UserAdmin
 
 from .models import User
 
+class UserAdmin(admin.ModelAdmin):
+    autocomplete_fields = ['randonneur']
 
-admin.site.register(User)
+
+admin.site.register(User, UserAdmin)
