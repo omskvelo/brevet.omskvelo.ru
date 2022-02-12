dd2a3c9c0a8399bb478811b11cc918f5d2bb8cd3
Avermakov <avermakov@list.ru>
2022-02-12 23:40:32 +0600

    Review header

--- brevet/users/models.py
+++ brevet/users/models.py
@@ -24,6 +24,9 @@ class User(AbstractUser):
     def __str__(self):
         return f"{self.last_name} {self.first_name} {self.phone_number} "
 
+    def get_username(self):
+        return f"{self.first_name} {self.last_name}"
+
     class Meta:
         db_table = 'auth_user'
 
