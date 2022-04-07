827b2e77ad15703a691af8552f648c0e62836ae6
Raccoonrider <avermakov@list.ru>
2022-04-07 17:58:21 +0600

    Add healthcheck

--- brevet/brevet/settings.py
+++ brevet/brevet/settings.py
@@ -1,8 +1,6 @@
 from pathlib import Path
 import os
 
-print ("Reading settings!")
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
