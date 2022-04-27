9d9d6203eb19a6e3225b3adfc79fc9f90597ad86
Raccoonrider <avermakov@list.ru>
2022-04-27 12:51:37 +0600

    Change e-mail provider to Yandex due to security restrictions on Google

--- brevet/brevet/settings.py
+++ brevet/brevet/settings.py
@@ -157,12 +157,14 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # E-mail configuration
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST = 'smtp.yandex.ru'
+EMAIL_USE_TLS = False
+EMAIL_USE_SSL = True
+EMAIL_PORT = 465
 EMAIL_HOST_PASSWORD = os.environ['EMAIL_HOST_PASSWORD']
-EMAIL_USE_TLS = True
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'audaxomsk@gmail.com'
-EMAIL_USE_SSL = False
+EMAIL_HOST_USER = os.environ['EMAIL_HOST_USER']
+SERVER_EMAIL = EMAIL_HOST_USER
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 # Security
 SESSION_COOKIE_SECURE = False
