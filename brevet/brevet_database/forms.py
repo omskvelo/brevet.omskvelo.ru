39ee8ade2d761e2ec89790aee3e6c8caa5e4c3c8
Raccoonrider <avermakov@list.ru>
2022-06-13 21:06:03 +0600

    Refactor result form validation

--- brevet/brevet_database/forms.py
+++ brevet/brevet_database/forms.py
@@ -2,12 +2,7 @@ import re
 
 from django import forms
 
-result_pattern = [
-    re.compile("^\d\d.\d\d$"),
-    re.compile("^\d\d\d\d$"),
-    re.compile("^\d.\d\d$"),
-    re.compile("^\d\d\d$"),
-    ]
+result_pattern = re.compile("^(\d{1,2})[^\d]?(\d{2})$")
 
 class ProtocolUploadForm(forms.Form):
     xls = forms.FileField(label="*.xls")
@@ -18,19 +13,16 @@ class AddResultForm(forms.Form):
     medal = forms.BooleanField(required=False)
 
     def __init__(self, data=None, *args, **kwargs):
-        """ Allows user to enter duration in HH:MM format instead of default HH:MM:SS format.
-        In case user uses different symbol for ":" , the result can still be valid.
-        In case user misses ":" entirely, the result can still be valid.
+        """ Allows user to enter time in HH[?]MM format instead of default HH:MM:SS format.
+        Any delimiter is valid.
         """
         if data:
             data = data.copy()
-            data['result'] = data['result'].strip()
-            if result_pattern[0].match(data['result']):
-                data['result'] = data['result'][0:2] + ":" + data['result'][3:] + ":00"
-            elif result_pattern[1].match(data['result']):
-                data['result'] = data['result'][0:2] + ":" + data['result'][2:] + ":00"
-            elif result_pattern[2].match(data['result']):
-                data['result'] = '0' + data['result'][0] + ":" + data['result'][2:] + ":00"
-            elif result_pattern[3].match(data['result']):
-                data['result'] = '0' + data['result'][0] + ":" + data['result'][1:] + ":00"
+            result = data['result'].strip()
+            result_match = result_pattern.match(result)
+            if result_match:
+                data['result'] = result_match.group(1) + ":" + result_match.group(2) + ":00"
+
+            print (data['result'])
+
         super().__init__(data, *args, **kwargs)
