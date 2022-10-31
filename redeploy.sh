1c34d0951326565ad5ce62d80a20e820277cb9c4
Raccoonrider <avermakov@list.ru>
2022-10-31 13:27:52 +0600

    Fix duplicate upstream?

--- redeploy.sh
+++ redeploy.sh
@@ -5,14 +5,14 @@ git pull
 docker-compose up -d
 
 # Switch to green
-docker exec brevetomskveloru-nginx-1 cp /etc/nginx/conf.d/nginx_green.conf /etc/nginx/conf.d/nginx.conf
+docker exec brevetomskveloru-nginx-1 cp /home/brevet/nginx_green.conf /etc/nginx/conf.d/nginx.conf
 docker exec brevetomskveloru-nginx-1 nginx -s reload
 
 docker-compose build web-blue
 docker-compose up -d
 
 # Switch to blue
-docker exec brevetomskveloru-nginx-1 cp /etc/nginx/conf.d/nginx_blue.conf /etc/nginx/conf.d/nginx.conf
+docker exec brevetomskveloru-nginx-1 cp /home/brevet/nginx_blue.conf /etc/nginx/conf.d/nginx.conf
 docker exec brevetomskveloru-nginx-1 nginx -s reload
 
 docker-compose build web-green
