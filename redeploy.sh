471dbc17e542508eacf9092fbfb0fe21658aafef
Raccoonrider <avermakov@list.ru>
2022-10-31 13:45:32 +0600

    Update nginx conf

--- redeploy.sh
+++ redeploy.sh
@@ -2,8 +2,6 @@
 cd /srv/brevet.omskvelo.ru
 git pull
 
-docker-compose up -d
-
 # Switch to green
 docker exec brevetomskveloru-nginx-1 cp /home/brevet/nginx_green.conf /etc/nginx/conf.d/nginx.conf
 docker exec brevetomskveloru-nginx-1 nginx -s reload
