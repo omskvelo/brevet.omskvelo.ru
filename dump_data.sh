67c8d39c7959af9d7ca86be4d37db714c4ea6b6a
Raccoonrider <avermakov@list.ru>
2022-11-22 16:36:06 +0600

    Update dump_data.sh

--- dump_data.sh
+++ dump_data.sh
@@ -1,8 +1,3 @@
 cd /srv/brevet.omskvelo.ru
 
-docker exec -it brevetomskveloru_web-blue_1 sh
-cd /home/brevet/web/brevet
-python manage.py dumpdata --natural-foreign --natural-primary -e contenttypes -e auth.Permission -e users.profile -e admin.logentry --indent 2 > dump.json
-exit
-
-docker cp brevetomskveloru_web-blue_1:/home/brevet/web/brevet/dump.json .
+docker exec brevetomskveloru_web-blue_1 cd python /home/brevet/web/brevet/manage.py dumpdata --natural-foreign --natural-primary -e contenttypes -e auth.Permission -e admin.logentry --indent 2 > dump.json
