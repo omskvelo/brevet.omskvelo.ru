1496fa77609626904d0efab9fe40429b058d62aa
Raccoonrider <avermakov@list.ru>
2022-11-22 22:13:38 +0600

    Update redeploy.sh

--- redeploy.sh
+++ redeploy.sh
@@ -3,9 +3,6 @@
 cd /srv/brevet.omskvelo.ru
 git pull
 
-# Make sure green is running
-docker-compose up -d
-
 # Switch to green
 docker exec brevetomskveloru_nginx_1 cp /home/brevet/nginx_green.conf /etc/nginx/conf.d/nginx.conf
 docker exec brevetomskveloru_nginx_1 nginx -s reload
@@ -19,6 +16,3 @@ docker exec brevetomskveloru_nginx_1 nginx -s reload
 
 docker-compose build web-green
 docker-compose up -d
-
-# Kill green
-docker stop brevetomskveloru_web-green_1
