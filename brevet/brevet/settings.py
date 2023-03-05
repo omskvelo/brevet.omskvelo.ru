5a6eaea67c2a7bcd725293a6632fcd09db896fa5
Raccoonrider <avermakov@list.ru>
2023-03-05 21:07:50 +0600

    Adding vk oauth...

--- brevet/brevet/settings.py
+++ brevet/brevet/settings.py
@@ -74,8 +74,6 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'social_django.context_processors.backends',
-                'social_django.context_processors.login_redirect',
                 'users.context_processors.access_oauth2_settings',
             ],
         },
