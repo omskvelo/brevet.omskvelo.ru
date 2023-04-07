91a9277af13c9ae45215b25d8f3933db951d9a6d
Raccoonrider <avermakov@list.ru>
2023-04-07 11:22:48 +0600

    Remove blue-green deployment

--- redeploy.sh
+++ redeploy.sh
@@ -3,16 +3,5 @@
 cd /srv/brevet.omskvelo.ru
 git pull
 
-# Switch to green
-docker exec brevetomskveloru_nginx_1 cp /home/brevet/nginx_green.conf /etc/nginx/conf.d/nginx.conf
-docker exec brevetomskveloru_nginx_1 nginx -s reload
-
-docker-compose build web-blue
-docker-compose up -d
-
-# Switch to blue
-docker exec brevetomskveloru_nginx_1 cp /home/brevet/nginx_blue.conf /etc/nginx/conf.d/nginx.conf
-docker exec brevetomskveloru_nginx_1 nginx -s reload
-
-docker-compose build web-green
+docker-compose build web
 docker-compose up -d
