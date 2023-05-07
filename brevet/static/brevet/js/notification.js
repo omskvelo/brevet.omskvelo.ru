e45d17fc1de4007894f61c97d303a2f70fc9cac6
Raccoonrider <avermakov@list.ru>
2023-05-08 02:17:14 +0600

    Update notifications

--- brevet/static/brevet/js/notification.js
+++ brevet/static/brevet/js/notification.js
@@ -1,6 +1,8 @@
 document.showNotification = function(message){
     const notificationDom = document.querySelector('.notification')
     notificationDom.innerText = message
+    notificationDom.style.display = 'block'
     notificationDom.style.opacity = 1
     setTimeout(()=>{notificationDom.style.opacity = 0}, 2500)
+    setTimeout(()=>{notificationDom.style.display = 'none'}, 2750)
 }
