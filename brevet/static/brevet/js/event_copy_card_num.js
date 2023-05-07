698d879d0caa0cdf0754552be009b4a6751805df
Raccoonrider <avermakov@list.ru>
2023-05-07 23:17:15 +0600

    Users can now copy card number to clipboard

---
+++ brevet/static/brevet/js/event_copy_card_num.js
@@ -0,0 +1,18 @@
+{
+    let targetTextBlock = document.querySelector('.insert-copy-btn')
+    let splitPattern = /(.*)(\d{16}|\d{4}\s\d{4}\s\d{4}\s\d{4})(.*)/
+    targetTextBlock.innerHTML = targetTextBlock.innerText.replace(splitPattern, "$1 <a>$2</a> $3")
+    targetTextBlock.classList.remove('insert-copy-btn')
+    let cardLink = targetTextBlock.querySelector('a')
+    cardLink.setAttribute('class', 'btn btn-light')
+
+    cardLink.addEventListener('click', function(event) {
+        navigator.clipboard.writeText(cardLink.innerText).then(
+            () => {document.showNotification("Скопировано в буфер обмена")},
+            () => {document.showNotification("Невозможно скопировать в буфер обмена")}
+        )
+    });
+
+
+
+}
